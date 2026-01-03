d6835d4dad6734b8a928053b30a1ae6f641c1357
Jacob Lee <75264826+ijiyu@users.noreply.github.com>
2026-01-03 09:10:46 -0500

    Added sorting

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from PIL import Image, ImageDraw
 import random
+import os
+import glob
 
 SCREEN_WIDTH = 2560
 SCREEN_HEIGHT = 1440
@@ -25,7 +27,16 @@ class Bar:
         canvas.rectangle(shape, fill=self.color)
 
 
-def main():
+def draw_bars(bars, filename):
+    output_dir = "frames"
+    image = Image.new("RGB", (SCREEN_WIDTH, SCREEN_HEIGHT), BACKGROUND_COLOR)
+    canvas = ImageDraw.Draw(image)
+    for bar in bars:
+        bar.draw(canvas)
+    image.save(os.path.join(output_dir, filename))
+
+
+def bubble_sort():
     x_positions = list(range(0, SCREEN_WIDTH, BAR_WIDTH))
     heights = [(x_position+BAR_WIDTH)*0.5 for x_position in x_positions]
     random.shuffle(heights)
@@ -35,15 +46,38 @@ def main():
         for x, h in zip(x_positions, heights)
     ]
 
-    random.shuffle(bars)
+    iteration = 0
+    output_dir = "frames"
+    os.makedirs(output_dir, exist_ok=True)
+    for file_path in glob.glob(os.path.join(output_dir, "*")):
+        if os.path.isfile(file_path):
+            os.remove(file_path)
+    draw_bars(bars, "0.jpg")
 
-    image = Image.new("RGB", (SCREEN_WIDTH, SCREEN_HEIGHT), BACKGROUND_COLOR)
-    canvas = ImageDraw.Draw(image)
+    for _ in range(len(bars)-1):
+        done = True
+        for i in range(len(bars)-1):
+            bars[i].color = "#ff0000"
+            bars[i+1].color = "#ff0000"
+            if bars[i].height > bars[i+1].height:
+                bars[i].height, bars[i+1].height = bars[i+1].height, bars[i].height
+                done = False
+            draw_bars(bars, f"{iteration}.jpg")
 
-    for bar in bars:
-        bar.draw(canvas)
+            iteration += 1
+            bars[i].color = "#ffffff"
+            bars[i+1].color = "#ffffff"
+        if done:
+            break
 
-    image.show()
+    for i in range(len(bars)):
+        bars[i].color = "#00ff00"
+        draw_bars(bars, f"{iteration}.jpg")
+        iteration += 1
+
+
+def main():
+    bubble_sort()
 
 
 if __name__ == "__main__":
